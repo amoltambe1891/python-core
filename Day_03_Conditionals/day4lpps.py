b35771d282aa5c69b3531fa25f443bd6d504c06e
unknown <amoltambe1891@gmail.com>
2026-07-19 09:04:02 -0900

    addesd new file

---
+++ Day_03_Conditionals/day4lpps.py
@@ -0,0 +1,7 @@
+passing_marks = 40
+student_marks = 38
+
+# Evaluates to a direct Boolean
+has_passed = student_marks >= passing_marks 
+
+print(f"Did the student pass the exam? : {has_passed}") # Prints: False
